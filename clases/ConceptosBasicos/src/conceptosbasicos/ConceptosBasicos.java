c83ca45eff3e350f9072a952e8095537aa2bd616
JairSalvador <jairsalvador71@aragon.unam.mx>
2022-03-01 12:53:19 -0600

    Sesion 1 marzo, Automovil

--- clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -4,6 +4,10 @@
  */
 package conceptosbasicos;
 
+import ico.fes.Automovil;
+import ico.fes.Silla;
+import java.awt.Color;
+
 /**
  *
  * @author calav
@@ -16,6 +20,23 @@ public class ConceptosBasicos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola de nuevo");
+        Silla s1 = new Silla();
+        System.out.println(s1);
+        System.out.println(s1.toString());
+
+        Automovil miVocho = new Automovil();
+        System.out.println(miVocho);
+        miVocho.setMarca("VW");
+        miVocho.setSubMarca("Sedan");
+        miVocho.setModelo(1970);
+        miVocho.setColor(Color.BLUE);
+        System.out.println(miVocho);
+
+        Automovil miMustang = new Automovil("Ford", "Mustang", 2010, Color.YELLOW);
+        System.out.println(miMustang);
+
+        Automovil miAkura = new Automovil("Akura", "NSX", 2013, Color.GRAY);
+        System.out.println(miAkura);
     }
 
 }
